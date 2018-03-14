a52f609cb292575202808fc9bb7ea7f7b4223387
Aziz Kosber <a.kosber@gmail.com>
2018-03-14 03:47:42 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = {radiohead: "weird fishes"};
+
+function updatePlaylist(playlist, artist, song) {
+  playlist[artist] = song
+
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+    delete playlist[artistName]
+    return playlist
+}
