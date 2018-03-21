4f63f912a02ec32eb8df11cfc859615634433417
Aziz Kosber <a.kosber@gmail.com>
2018-03-21 20:17:37 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = {Radiohead: 'Present Tense'};
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+}
